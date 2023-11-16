d71970ec8d52edf009af3ece9a22bfc1c0e879b9
Huỳnh Công Lợi <127321058+hcongloi@users.noreply.github.com>
2023-11-16 14:37:13 +0700

    Delete BT_CaNhan/Huynh-Cong-Loi directory

--- BT_CaNhan/Huynh-Cong-Loi/PHP&MYSQL/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $servername ="localhost";
-    $username ="root";
-    $password="";
-    $dbname="quanly_ban_sua";
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
- ?>
